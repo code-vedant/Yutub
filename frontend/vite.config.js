1d8a764ddc0d7ee06004f7dcb22d053f6f06b772
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-09-29 18:44:47 +0530

    cspCheck

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { cspPlugin } from 'vite-plugin-csp';
+// import { cspPlugin } from 'vite-plugin-csp';
 
 export default defineConfig({
   plugins: [
     react(),
-    cspPlugin({
-      'default-src': ['self'],
-      'style-src': ['self', "'unsafe-inline'"],
-    }),
+    // cspPlugin({
+    //   'default-src': ['self'],
+    //   'style-src': ['self', "'unsafe-inline'"],
+    // }),
   ],
 });
