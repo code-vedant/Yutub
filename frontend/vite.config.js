3eb053f532dda144d91e5f468c64d7c51ae18af2
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-09-29 18:58:08 +0530

    cspfont

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { cspPlugin } from 'vite-plugin-csp';
+import { cspPlugin } from 'vite-plugin-csp';
 
 export default defineConfig({
   plugins: [
     react(),
-    // cspPlugin({
-    //   'default-src': ['self'],
-    //   'style-src': ['self', "'unsafe-inline'"],
-    // }),
+    cspPlugin({
+      'default-src': ['self'],
+      'style-src': ['self', "'unsafe-inline'", 'https://fonts.googleapis.com'],
+      'font-src': ['self', 'https://fonts.gstatic.com'], // Add this line
+    }),
   ],
 });
