93c44e1921e99f23eedffeec2e6da6823097a5f6
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-06-21 01:19:51 +0530

    photo ui

--- frontend/src/components/Photos/PhotoContainer.jsx
+++ frontend/src/components/Photos/PhotoContainer.jsx
@@ -1,15 +1,34 @@
+import { Link } from "react-router-dom";
 import "../../style/photoContainer.css";
+import { CiBookmark } from "react-icons/ci";
+import { IoHeartOutline } from "react-icons/io5";
 
-
-export default function PhotoContainer({photo}) {
-    console.log("PhotoContainer component is being rendered",photo);
+export default function PhotoContainer({ photo }) {
   return (
     <div className="photo-container">
-        <img src={photo.photoFile} alt="" />
-        <div className="photo-info">
+      <img src={photo.photoFile} alt="" />
+      <div className="photo-options">
+        <button>
+          <CiBookmark />
+        </button>
+        <button>
+          <IoHeartOutline />
+        </button>
+      </div>
+
+      <div className="photo-info">
+        <div className="photo-info-btm">
+          <div className="photo-info-btm-left">
+            <img src={photo.owner.avatar} alt="" />
+          </div>
+          <div className="photo-info-btm-right">
             <h3>{photo.title}</h3>
-            <p>{photo.description}</p>
-            </div>
+            <Link to={`/profile/${photo.owner._id}`}>
+              @{photo.owner.username}
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
