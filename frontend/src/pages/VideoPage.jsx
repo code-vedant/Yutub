e4d26f7d7669644f929faae1023c5d6a76ee2385
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-06-29 08:58:07 +0530

    name fix

---
+++ frontend/src/pages/VideoPage.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import VideoService from '../service/video'
+import VideoContainer from '../components/VideoComponents/VideoContainer'
+import '../style/videopage.css'
+
+export default function VideoPage() {
+
+  const [video,setVideo] = useState([])
+  const [error,setError] = useState("")
+
+  const getVideos = async () => {
+    setError("")
+    try {
+      const res = await VideoService.getAllVideos()
+      setVideo(res.data.docs)
+
+    } catch (error) {
+      setError(error.response.data.message)
+    }
+  }
+
+  useEffect(()=>{
+    getVideos()
+  },[])
+
+
+  return (
+    <section className='videopage-main'>
+      {error && <p>{error}</p>}
+      {video.map((vid)=> 
+      <VideoContainer key={vid._id} video={vid} />
+      )}
+    </section>
+  )
+}
