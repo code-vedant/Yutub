6a6879a7a85a47ff74c447eaeb6ef3c989f46af0
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-07-22 22:06:26 +0530

    https error fix

--- frontend/src/components/VideoComponents/VideoPlayer.jsx
+++ frontend/src/components/VideoComponents/VideoPlayer.jsx
@@ -1,7 +1,7 @@
 const VideoPlayer = ({ url }) => {
   return (
     <div className="video-player">
-      <video src={url} controls width="100%" height="100%" className="react-video-player" />
+      <video src={url.replace(/^http:\/\//, 'https://')} controls width="100%" height="100%" className="react-video-player" />
     </div>
   );
 };
